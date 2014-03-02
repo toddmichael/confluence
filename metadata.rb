f6cb0fb17a3959daea2440c5956f793fccb66a8c
Todd Michael Bushnell <toddmichael@gmail.com>
2014-03-01 23:21:48 -0800

    bump version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "systems@secondmarket.com"
 license          "Apache 2.0"
 description      "Installs/Configures Atlassian Confluence"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 depends 'database', '~> 1.6.0'
 depends 'java', '~> 1.20.0'
